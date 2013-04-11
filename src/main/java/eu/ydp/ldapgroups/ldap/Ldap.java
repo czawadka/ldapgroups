ee12d30b0a3addb99814dedeb8fe509896ae5b08
czawadka <Cezary.Zawadka@ydp.eu>
2013-04-11 15:30:55 +0200

    tests for Ldap

--- src/main/java/eu/ydp/ldapgroups/ldap/Ldap.java
+++ src/main/java/eu/ydp/ldapgroups/ldap/Ldap.java
@@ -6,55 +6,87 @@ import org.springframework.ldap.core.LdapTemplate;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.naming.NamingException;
-import javax.naming.directory.Attribute;
-import javax.naming.directory.Attributes;
+import javax.naming.directory.*;
 import java.util.*;
 
-@Named
 public class Ldap {
     static public final String ATTR_DISTINGUISHED_NAME = "distinguishedName";
     static public final String ATTR_MEMBER = "member";
     static public final String ATTR_SAM_ACCOUNT_NAME = "sAMAccountName";
+    static public final String ATTR_NAME = "name";
 
     @Inject
     LdapTemplate ldapTemplate;
+    String baseDn;
 
-    public Set<String> getMembers(String groupName) {
+    public Collection<String> getMembers(String groupName) {
         try {
-            Set<String> members = null;
+            Collection<String> members = null;
             Attributes group = getGroup(groupName);
             if (group!=null) {
-                List<String> memberDns = getMemberDnsFromGroup(group);
-                List<Attributes> memberEntries = searchByAttr(ATTR_DISTINGUISHED_NAME, memberDns);
-                Map<String, String> dnLoginMap = getEntryValueMapByAttr(memberEntries, ATTR_SAM_ACCOUNT_NAME);
-                members = new LinkedHashSet<String>(dnLoginMap.values());
+                List<String> memberDns = getValuesForEntry(group, ATTR_MEMBER);
+                members = getValueForEntriesWithAttr(ATTR_DISTINGUISHED_NAME, memberDns,
+                        ATTR_SAM_ACCOUNT_NAME);
             }
-            return members;
+            return members!=null ? new LinkedHashSet<String>(members) : null;
         } catch (NamingException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public boolean setMembers(String groupName, Collection<String> members) {
+        try {
+            Attributes group = getGroup(groupName);
+            if (group==null) {
+                return false;
+            }
+            String groupDn = group.get(ATTR_DISTINGUISHED_NAME).get().toString();
+            Collection<String> memberDns = getValueForEntriesWithAttr(
+                    ATTR_SAM_ACCOUNT_NAME, members,
+                    ATTR_DISTINGUISHED_NAME);
+            BasicAttribute memberAttr = new BasicAttribute(ATTR_MEMBER);
+            for(String memberDn : memberDns) {
+                memberAttr.add(memberDn);
+            }
+            ModificationItem removeItem = new ModificationItem(DirContext.REMOVE_ATTRIBUTE, new BasicAttribute(ATTR_MEMBER));
+            ModificationItem replaceItem = new ModificationItem(DirContext.REPLACE_ATTRIBUTE, memberAttr);
+            ldapTemplate.modifyAttributes(groupDn, new ModificationItem[] {removeItem, replaceItem});
+
+            return true;
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getBaseDn() {
+        return baseDn;
+    }
+
+    public void setBaseDn(String baseDn) {
+        this.baseDn = baseDn;
+    }
+
     private Attributes getGroup(String groupName) {
-        String filter = "(&(objectClass=group)(name="+groupName+"))";
+        String filter = "(&(objectClass=group)("+ATTR_NAME+"="+groupName+"))";
         return searchFirst(filter);
     }
 
-    private List<String> getMemberDnsFromGroup(Attributes group) throws NamingException {
-        Attribute memberAttribute = group.get(ATTR_MEMBER);
+    private List<String> getValuesForEntry(Attributes entry, String returnedAttrName) throws NamingException {
+        List<String> values = new ArrayList<String>();
 
-        int valueCount = memberAttribute.size();
-        List<String> memberDns = new ArrayList<String>(valueCount);
-
-        for (int i = 0; i < valueCount; i++) {
-            String value = memberAttribute.get(i).toString();
-            memberDns.add(value);
+        Attribute attribute = entry.get(returnedAttrName);
+        if (attribute!=null) {
+            int valueCount = attribute.size();
+            for (int i = 0; i < valueCount; i++) {
+                String value = attribute.get(i).toString();
+                values.add(value);
+            }
         }
 
-        return memberDns;
+        return values;
     }
 
-    private List<Attributes> searchByAttr(String attrName, List<String> values) {
+    private List<Attributes> searchByAttr(String attrName, Collection<String> values) {
         if (values.isEmpty())
             return Collections.EMPTY_LIST;
 
@@ -72,21 +104,27 @@ public class Ldap {
         return sb.toString();
     }
 
-    private Map<String, String> getEntryValueMapByAttr(List<Attributes> entries, String attributeName) throws NamingException {
-        Map<String, String> dnAttributeMap = new LinkedHashMap<String, String>();
+    private Map<Attributes, String> getValueForEntries(List<Attributes> entries, String returnedAttrName) throws NamingException {
+        Map<Attributes, String> dnValueMap = new LinkedHashMap<Attributes, String>();
 
         for(Attributes entry : entries) {
-            Attribute attribute = entry.get(attributeName);
-            String dn = entry.get(ATTR_DISTINGUISHED_NAME).get().toString();
+            Attribute attribute = entry.get(returnedAttrName);
             String attributeValue = attribute.get().toString();
-            dnAttributeMap.put(dn, attributeValue);
+            dnValueMap.put(entry, attributeValue);
         }
 
-        return dnAttributeMap;
+        return dnValueMap;
+    }
+
+    private Collection<String> getValueForEntriesWithAttr(String withAttrName,
+                                                          Collection<String> withAttrValues,
+                                                          String returnedAttrName) throws NamingException {
+        List<Attributes> entries = searchByAttr(withAttrName, withAttrValues);
+        return getValueForEntries(entries, returnedAttrName).values();
     }
 
     private List<Attributes> search(String filter) {
-        return ldapTemplate.search("", filter, AttributesAttributesMapper.INSTANCE);
+        return ldapTemplate.search(baseDn, filter, AttributesAttributesMapper.INSTANCE);
     }
 
     private Attributes searchFirst(String filter) {
@@ -95,6 +133,12 @@ public class Ldap {
         if (it.hasNext())
             return it.next();
         return null;
+    }
+
+    private Set<String> createCaseInsensitiveSet(Collection<String> values) {
+        Set<String> newValues = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
+        newValues.addAll(values);
+        return newValues;
     }
 
     static class AttributesAttributesMapper implements AttributesMapper {
