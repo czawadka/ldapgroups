c20387cda84b84beabaa34c72a53c80d1d053760
czawadka <Cezary.Zawadka@ydp.eu>
2013-04-30 16:25:07 +0200

    log start/stop in worker manager

--- src/main/java/eu/ydp/ldapgroups/worker/WorkerManager.java
+++ src/main/java/eu/ydp/ldapgroups/worker/WorkerManager.java
@@ -2,12 +2,15 @@ package eu.ydp.ldapgroups.worker;
 
 import com.yammer.dropwizard.lifecycle.Managed;
 import com.yammer.dropwizard.util.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class WorkerManager implements Managed {
+    Logger log = LoggerFactory.getLogger(getClass());
     ScheduledExecutorService scheduler;
     Runnable worker;
     Duration period;
@@ -30,11 +33,13 @@ public class WorkerManager implements Managed {
 
     @Override
     public void start() throws Exception {
+        log.info("Starting worker {} run every {} seconds", worker.toString(), period);
         scheduledFuture = scheduler.scheduleAtFixedRate(worker, 0, period.getQuantity(), period.getUnit());
     }
 
     @Override
     public void stop() throws Exception {
+        log.info("Stop worker {}", worker.toString());
         scheduledFuture.cancel(true);
     }
 }
